0326ca73316ae6095a1ad1c823a91690c56fe36f
Ahn Hyo Won <ahnhw20@naver.com>
2025-03-25 14:39:41 +0900

    Add data file

---
+++ dict.py
@@ -0,0 +1,21 @@
+my_dict1 = {}
+print(my_dict1,type(my_dict1))
+
+my_dict2 = {0:1}
+print(my_dict2,type(my_dict1))
+
+my_dict3 = {'이름':'앨리스','나이':27,'직업':['개발자','학생'],'메일수신동의여부': True}
+print(my_dict3)
+my_dict3['성별'] = '여성'
+print(my_dict3)
+my_dict3['나이'] = 28
+print(my_dict3)
+
+print('-------------------')
+
+my = {'나이': 25, '직업': '학생'}
+print(my)
+my['번호'] = '010-5292-2349'
+print(my)
+
+
